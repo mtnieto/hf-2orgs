b77930f31ccafd3b774a4a8d863eded64a36c277
Mt <mtnietotcpsi@gmail.com>
2017-10-10 10:46:45 +0200

    added query and invoke chaincode

---
+++ scripts/07-query.sh
@@ -0,0 +1,31 @@
+ORDERER_CA=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/regulatory.com/orderers/orderer.regulatory.com/msp/tlscacerts/tlsca.regulatory.com-cert.pem
+CORE_PEER_LOCALMSPID="fcaMSP"
+CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/fca.regulatory.com/peers/peer0.fca.regulatory.com/tls/ca.crt
+CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/fca.regulatory.com/users/Admin@fca.regulatory.com/msp
+CORE_PEER_ADDRESS=peer0.fca.regulatory.com:7051
+CHANNEL_NAME=fcachannel
+CORE_PEER_TLS_ENABLED=true
+
+
+
+verifyResult () {
+	if [ $1 -ne 0 ] ; then
+		echo "!!!!!!!!!!!!!!! "$2" !!!!!!!!!!!!!!!!"
+                echo "================== ERROR !!! FAILED to execute End-2-End Scenario =================="
+		echo
+   		exit 1
+	fi
+}
+queryChaincode () {
+	# while 'peer chaincode' command can get the orderer endpoint from the peer (if join was successful),
+	# lets supply it directly as we know it using the "-o" option
+
+	peer chaincode query -C $CHANNEL_NAME -n mycontract -c '{"Args":["query","a"]}' >&log.txt
+	res=$?
+	cat log.txt
+	verifyResult $res "Chaincode instantiation on PEER on channel '$CHANNEL_NAME' failed"
+	echo "===================== Chaincode Instantiation on PEER on channel '$CHANNEL_NAME' is successful ===================== "
+	echo
+}
+
+queryChaincode
